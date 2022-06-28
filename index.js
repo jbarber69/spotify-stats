ce4afccfa0a4c0d9c4f09505f93286c30433b841
Jack Barber <jack@theblankhand.co.uk>
2022-06-28 19:41:10 +0100

    missing mf questionmark

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ app.get('/callback', (req, res) => {
                     expires_in
                 });
 
-                res.redirect(`${FRONTEND_URI}/${queryParams}`);
+                res.redirect(`${FRONTEND_URI}/?${queryParams}`);
 
             } else {
                 res.redirect(`/?${querystring.stringify({ error: 'invalid_token' })}`);
